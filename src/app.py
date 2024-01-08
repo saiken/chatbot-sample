f82be938ecea33d4f316d31bdf09c13da00e643e
saiken <saiken1212@gmail.com>
2024-01-08 21:55:13 +0900

    add XHRRequest(axios)

--- src/app.py
+++ src/app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 app = Flask(__name__)
+app.config["JSON_AS_ASCII"] = False
 
 @app.route('/')
-def hello():
+def index():
     html = render_template('index.html')
     return html
+
+@app.route('/api/sendMessage', methods=['POST'])
+def sendMessage():
+    return jsonify({"role": "assistant", "message": "すごくいいですね"})
 
 if __name__ == "__main__":
     app.run()
